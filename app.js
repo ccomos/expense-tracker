566372b84ceb919239da741f5a67094b55dd063a
wei <fhaowei@yahoo.com.tw>
2020-05-23 15:02:26 +0800

    new creat

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const port = 3000
+const app = express()
+const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+const Record = require('./models/record')
+
+mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.addListener('open', () => {
+  console.log('mongodb connected!')
+})
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`App is listening on localhost:${port}`)
+})
