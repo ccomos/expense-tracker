8254fd89a3b7fec23d5275a3b00928288d32272e
wei <fhaowei@yahoo.com.tw>
2020-05-23 21:24:01 +0800

    feat: add index page

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ db.addListener('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.render('index')
 })
