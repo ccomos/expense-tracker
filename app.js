bedaf0bdf69900edd3848bef883348bc5e305f2a
wei <fhaowei@yahoo.com.tw>
2020-05-24 11:52:05 +0800

    feat: add edit function

--- app.js
+++ app.js
@@ -44,6 +44,31 @@ app.post('/creat', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.get('/records/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .lean()
+    .then((records) => res.render('edit', { records }))
+    .catch(error => console.log(error))
+})
+
+app.post('/records/:id', (req, res) => {
+  const id = req.params.id
+  const editRecord = req.body
+  if (editRecord.category === "家居物業") { editRecord.categoryIcon = 'fas fa-home' }
+  if (editRecord.category === "交通出行") { editRecord.categoryIcon = 'fas fa-shuttle-van' }
+  if (editRecord.category === "休閒娛樂") { editRecord.categoryIcon = 'fas fa-grin-beam' }
+  if (editRecord.category === "餐飲食品") { editRecord.categoryIcon = 'fas fa-utensils' }
+  if (editRecord.category === "其他") { editRecord.categoryIcon = 'fas fa-pen' }
+  return Record.findById(id)
+    .then(record => {
+      record = Object.assign(record, editRecord) //Object.assign(target, sources) 複製Sources所有的屬性至目標 target物件
+      return record.save()
+    })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.listen(port, () => {
   console.log(`App is listening on localhost:${port}`)
 })
