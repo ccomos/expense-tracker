4756f776b3063b4002e49ddf7aceaa2c939e4bf0
wei <fhaowei@yahoo.com.tw>
2020-05-24 10:58:30 +0800

    feat: add add function

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const app = express()
 const exphbs = require('express-handlebars')
 const Record = require('./models/record')
 const Category = require('./models/category')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
   let totalAmount = 0
@@ -22,7 +26,22 @@ app.get('/', (req, res) => {
       res.render('index', { records, totalAmount })
     })
     .catch(error => console.error(error))
-  //res.render('index')
+})
+
+app.get('/creat', (req, res) => {
+  res.render('creat')
+})
+
+app.post('/creat', (req, res) => {
+  const creatRecord = req.body
+  if (creatRecord.category === "家居物業") { creatRecord.categoryIcon = 'fas fa-home' }
+  if (creatRecord.category === "交通出行") { creatRecord.categoryIcon = 'fas fa-shuttle-van' }
+  if (creatRecord.category === "休閒娛樂") { creatRecord.categoryIcon = 'fas fa-grin-beam' }
+  if (creatRecord.category === "餐飲食品") { creatRecord.categoryIcon = 'fas fa-utensils' }
+  if (creatRecord.category === "其他") { creatRecord.categoryIcon = 'fas fa-pen' }
+  return Record.create(creatRecord)
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
