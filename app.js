e101515dfec2c5c45f638f3efc521758540940ed
wei <fhaowei@yahoo.com.tw>
2020-05-24 12:12:37 +0800

    feat: add delete function

--- app.js
+++ app.js
@@ -69,6 +69,14 @@ app.post('/records/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.delete('/records/:id', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then(record => record.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.listen(port, () => {
   console.log(`App is listening on localhost:${port}`)
 })
---
+++ public/javascripts/main.js
@@ -0,0 +1,4 @@
+//刪除前再次確認
+function deleteCheck() {
+  return window.confirm('確認要刪除嗎? / Would you like to delete?')
+}
